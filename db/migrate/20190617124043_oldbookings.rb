7d725bb53ac1c9ace383770617e35b9af39bb3c0
venkateskkarni <venkateshkarni1996@gmail.com>
2019-06-18 12:37:44 +0530

    changing bill

---
+++ db/migrate/20190617124043_oldbookings.rb
@@ -0,0 +1,6 @@
+class Oldbookings < ActiveRecord::Migration[5.2]
+
+  def change
+  end
+    add_column :cameras, :bill, :ineger
+end
---
+++ db/migrate/20190617132318_add_album_type.rb
@@ -0,0 +1,4 @@
+class AddAlbumType < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
